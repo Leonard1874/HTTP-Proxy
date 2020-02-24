2221b12197114bdf4d9b55be7d6adc4494a341f7
ww165 <ww165@duke.edu>
2020-02-24 18:59:37 -0500

    chache

--- http-proxy.cpp
+++ http-proxy.cpp
@@ -1,58 +1,4 @@
 #include "proxy.hpp"
-#include "logger.hpp"
-#include "cache.hpp"
-int Proxy::listenBrowser(const char* hostname, const char* port){
-  if(setupSocket(hostname,port) < 0){
-    std::cerr << "setup socket to browser error" << std::endl;
-    return EXIT_FAILURE;
-  }
-  if(acceptSocket() < 0){
-    std::cerr << "accept from browser" << std::endl;
-    return EXIT_FAILURE;
-  }
-  return EXIT_SUCCESS;
-}
-
-int Proxy::getRequest(std::string& request){
-  if(!recieve(getSockfdB(),request)){
-    std::cerr << "accept from browser" << std::endl;
-    return EXIT_FAILURE;
-  }
-  return EXIT_SUCCESS;
-}
-
-int Proxy::sendCacheBrowser(const std::string& cachedInfo){
-  if(!Send(getSockfdB(),cachedInfo)){
-    std::cerr << "send to browser" << std::endl;
-    return EXIT_FAILURE;
-  }
-  return EXIT_SUCCESS;
-}
-
-int Proxy::getServerSendBrowser(const std::string& hostName, const std::string& requestInfo, std::string& getInfo){
-  if(connectToSocket(hostName.c_str(),"80") < 0){ // port to be check
-    std::cerr << "connect to origin host error" << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  if(!Send(getSockfdO(),requestInfo.c_str())){
-    std::cerr << "send to origin error" << std::endl;
-    return EXIT_FAILURE;
-  }
-  
-  if(!recieve_origin(getSockfdO(),getInfo)){
-    std::cerr << "recv from origin error" << std::endl;
-    return EXIT_FAILURE;
-  }
-  
-  if(!Send(getSockfdB(),getInfo)){
-    std::cerr << "send to browser" << std::endl;
-    return EXIT_FAILURE;
-  }  
-  return EXIT_SUCCESS;
-}
-
-
 
 int main(int argc, char* argv[]){
   if(argc < 3){
@@ -60,64 +6,25 @@ int main(int argc, char* argv[]){
   }
   const char *hostname = argv[1];
   const char *port     = argv[2];
-  Proxy myProxy;
-  Cache myCache(20);
-  Timer myTimer;
-  int ID = 0;
-  logger my_logger;
+  Proxy myProxy(hostname,port);
   
   while(true){
-    if(myProxy.listenBrowser(hostname, port)){
-      std::cerr <<"listen error!"<< std::endl;
+    std::string requestInfo;
+    if(myProxy.lisenClient(requestInfo)){
       return EXIT_FAILURE;
     }
-
-    std::string requestInfo;
-    while(requestInfo.empty()){
-      if(myProxy.getRequest(requestInfo)){
-        std::cerr << "get request error" << std::endl;
-        return EXIT_FAILURE;
-      }
-    }
-    std::cout << "req: " <<requestInfo << std::endl;
-
     Request reqObj(requestInfo);
-    /*check request*/
-    /*check cache*/
-    
     std::string cached = "";
     if(reqObj.getType() == "GET"){
-      cached = myCache.get(reqObj.getKey(), myTimer.getCurrentSec());
-    }    
-    if(!cached.empty()){
-      std::cout << "****************Cache: found*******************" << std::endl;
-      //std::cout << cached << std::endl;
-      if(myProxy.sendCacheBrowser(cached)){
-        std::cerr <<"get error!"<< std::endl;
-        return EXIT_FAILURE;
-      }
-      my_logger.printCache("in cache", ID);
-      /*in cache situations*/
+      myProxy.handleGet(reqObj);
+    }
+    else if(reqObj.getType() == "POST"){
+      myProxy.handlePost(reqObj);
     }
     else{
-      /*logger*/ 
-      std::string getInfo;
-      if(myProxy.getServerSendBrowser(reqObj.getHostname(), reqObj.getRequestInfo(), getInfo)){
-        std::cerr <<"get error!"<< std::endl;
-        return EXIT_FAILURE;
-      }
-      std::cout << "res: " <<getInfo << std::endl;
-      Response resObj(getInfo,reqObj.getType(),myTimer.getCurrentSec());
-      /*update cache*/
-      if(resObj.canCache()){
-        myCache.put(reqObj.getKey(),resObj);
-      }
-      my_logger.printCache("not in cache", ID);
-      my_logger.getrequest(ID, reqObj);
-      my_logger.printlogline();
+      continue;
     }
     myProxy.closeSockfds();
-  }
-  
+  }  
   return EXIT_SUCCESS;
 }
--- proxy.hpp
+++ proxy.hpp
@@ -1,337 +1,82 @@
-#include <cstdio>
-#include <cstdlib>
-#include <unistd.h>
-#include <errno.h>
-#include <cstring>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <iostream>
-#include <sstream>
-#include <vector>
-#include <algorithm>
-#define MAXDATASIZE 100
+#include "Webtoolkit.hpp"
+#include "logger.hpp"
+#include "cache.hpp"
+
 class Proxy{
 private:
-  int sockfd_own;
-  int sockfd_to_browser;
-  int sockfd_to_origin;
-  std::vector<std::string> stringFromBrowser;
-public:
-  Proxy() = default;
-  void closeSockfds(){
-    close(sockfd_own);
-    close(sockfd_to_browser);
-    close(sockfd_to_origin);
-  }
-  
-  int getSockfdOwn(){
-    return sockfd_own;
-  }
-
-  int getSockfdB(){
-    return sockfd_to_browser;
-  }
-
-  int getSockfdO(){
-    return sockfd_to_origin;
-  }
+  const char* hostname;
+  const char* port;
+  WebToolKit myKit;
+  Timer myTimer;
+  logger myLogger;
+  Cache myCache;
+  int ID;
   
 public:
-  int listenBrowser(const char* hostname, const char* port);
-  int getRequest(std::string& reuqest);
-  int getServerSendBrowser(const std::string& hostName, const std::string& requestInfo, std::string& getInfo);
-  int sendCacheBrowser(const std::string& cachedInfo);
-private:
-  bool Send(int sendFd, std::string toSend){
-    if (send(sendFd, toSend.c_str(), strlen(toSend.c_str()), 0) == -1){ 
-      std::perror("send port number");
-      return false;
+  Proxy(const char* rhostname, const char* rport):hostname(rhostname), port(rport), myCache(20){}
+
+  int lisenClient(std::string& requestInfo){
+    if(myKit.listenBrowser(hostname, port)){
+      std::cerr <<"listen error!"<< std::endl;
+      return EXIT_FAILURE;
     }
-    return true;
-  }
 
-  int getLength(std::string& recv){
-    size_t pos = recv.find("Content-Length: ");
-    if(pos == std::string::npos){
-      std::cerr << "can't find length!" << std::endl;
-      return -1;
+    while(requestInfo.empty()){
+      if(myKit.getRequest(requestInfo)){
+        std::cerr << "get request error" << std::endl;
+        return EXIT_FAILURE;
+      }
+    }
+    std::cout << "req: " <<requestInfo << std::endl;
+    return EXIT_SUCCESS;
+  }
+  
+  int handleGet(Request& reqObj){
+    std::string cached = myCache.get(reqObj.getKey(), myTimer.getCurrentSec());
+    if(!cached.empty()){
+      std::cout << "****************Cache: found*******************" << std::endl;
+      //std::cout << cached << std::endl;
+      if(myKit.sendCacheBrowser(cached)){
+        std::cerr <<"get error!"<< std::endl;
+        return EXIT_FAILURE;
+      }
+      myLogger.printCache("in cache", ID);
+      /*in cache situations*/
+      return EXIT_SUCCESS;
     }
     else{
-      std::string lengthStr;
-      while(recv[pos]!='\n'){
-        if(isdigit(recv[pos])){
-          lengthStr += recv[pos];
-        }
-        pos ++;
+      /*update cache*/
+      std::string getInfo;
+      if(myKit.getServerSendBrowser(reqObj.getHostname(),reqObj.getRequestInfo(),getInfo)){
+        return EXIT_FAILURE;
       }
-      std::cout << lengthStr << std::endl;
-      return std::stoi(lengthStr);
-    }
-  }
-
-  int getHeadLength(std::string& recv){
-    size_t pos = recv.find("\r\n\r\n");
-    if(pos == std::string::npos){
-      std::cerr << "can't find empty line!" << std::endl;
-      return -1;
-    }
-    return pos;
-  }
-  
-  bool recieve_origin(int sockfd, std::string& toGet){
-    int numbytes = 0;
-    int recvNum = 0;
-    int allLen = 0;
-    int len = 0;
-    int headlen = 0;
-    int hasGot = 0;
-    char temp[65536];
-    while(true){
-   
-      if((numbytes = recv(sockfd, temp, 65535, 0)) == -1) {
-        std::perror("recv");
-        return false;
-      }
-      hasGot += numbytes;
-      std::string tempStr(temp);
-      //std::cout << "---------------------------" << std::endl;
-      //std::cout << tempStr << std::endl;
-      if(!recvNum){
-        if(tempStr.find("HTTP/1.1 200 OK") == std::string::npos){
-          allLen = 0;
-        }
-        else{
-          len = getLength(tempStr);
-          if(len < 0){
-            return false;
-          }
-          headlen = getHeadLength(tempStr);
-          if(headlen < 0){
-            return false;
-          }
-          allLen = headlen + len;
-        }
-        recvNum ++;
-      }
+      std::cout << getInfo << std::endl;
+      Response resObj(getInfo,reqObj.getType(),myTimer.getCurrentSec());
       
-      if(hasGot >= allLen){
-        toGet += tempStr;	
-        break;
+      if(resObj.canCache()){
+        myCache.put(reqObj.getKey(),resObj);
       }
-      else{
-        toGet += tempStr;
-      }
+      myLogger.printCache("not in cache", ID);
+      myLogger.getrequest(ID, reqObj);
+      myLogger.printlogline();
+      return EXIT_SUCCESS;
     }
-    return true;
-  }
-  
-  bool recieve(int sockfd, std::string& toGet){
-    int numbytes = 0;
-    char temp[65536];
-    while(true){
-      memset(temp,'\0',sizeof(temp));
-      std::string endMark = "\r\n\r\n";
-      if((numbytes = recv(sockfd, temp, 65535, 0)) == -1) {
-        std::perror("recv");
-        return false;
-      }
-      std::string tempStr(temp);
-      if(tempStr.find(endMark)!=std::string::npos){
-        toGet += tempStr;
-        //std::cout << "end! " <<toGet << std::endl;
-        break;
-      }
-      else{
-        //std::cout << toGet << std::endl;
-        toGet += tempStr;
-      }
-    }
-    return true;
   }
 
-  int acceptSocket(){
-    struct sockaddr_storage connector_addr;
-    socklen_t sin_size;
-    sin_size = sizeof connector_addr;
-    sockfd_to_browser = accept(sockfd_own, (struct sockaddr *)&connector_addr, &sin_size);
-    if (sockfd_to_browser == -1) {
-      std::perror("accept");
-      return -1;
+  int handlePost(Request& reqObj){
+    std::string getInfo;
+    if(myKit.getServerSendBrowser(reqObj.getHostname(),reqObj.getRequestInfo(),getInfo)){
+      return EXIT_FAILURE;
     }
-    return 0;
-  }
-  
-  int connectToSocket(const char* hostname, const char* port){
-    struct addrinfo host_info;
-    struct addrinfo *host_info_list;
-
-    memset(&host_info, 0, sizeof(host_info));
-    host_info.ai_family   = AF_UNSPEC;
-    host_info.ai_socktype = SOCK_STREAM;
-    host_info.ai_flags = AI_PASSIVE;
-
-    // std::cout << hostname << ":" << port << std::endl;
-  
-    int status = getaddrinfo(hostname, port, &host_info, &host_info_list);
-    if (status != 0) {
-      std::cerr << gai_strerror(status) << std::endl;
-      std::cerr << "Error: cannot get address info for host" << std::endl;
-      std::cerr << "(" << hostname << "," << port << ")" << std::endl;
-      return -1;
-    } 
-  
-    sockfd_to_origin = socket(host_info_list->ai_family, 
-                              host_info_list->ai_socktype, 
-                              host_info_list->ai_protocol);
-    if (sockfd_to_origin == -1) {
-      std::perror("build socket");
-      std::cerr << "Error: cannot create socket" << std::endl;
-      std::cerr << "  (" << hostname << ":" << port << ")" << std::endl;
-      return -1;
+    std::cout << getInfo << std::endl;
+    Response resObj(getInfo,reqObj.getType(),myTimer.getCurrentSec());
+    if(resObj.canCache()){
+      myCache.put(reqObj.getKey(),resObj);
     }
-  
-    status = connect(sockfd_to_origin, host_info_list->ai_addr, host_info_list->ai_addrlen);
-    if (status == -1) {
-      std::perror("connect client");
-      std::cerr << "Error: cannot connect to socket" << std::endl;
-      std::cerr << "  (" << hostname << ":" << port << ")" << std::endl;
-      return -1;
-    }
-    freeaddrinfo(host_info_list);
-    return 0;
+    return EXIT_SUCCESS;
   }
 
-  int setupSocket(const char* hostname, const char* port){
-    int status;
-    struct addrinfo host_info;
-    struct addrinfo *host_info_list;
-
-    memset(&host_info, 0, sizeof(host_info));
-
-    host_info.ai_family   = AF_UNSPEC;
-    host_info.ai_socktype = SOCK_STREAM;
-    host_info.ai_flags    = AI_PASSIVE;
-
-    status = getaddrinfo(hostname, port, &host_info, &host_info_list);
-  
-    if (status != 0) {
-      std::cerr << "Error: cannot get address info for host" << std::endl;
-      std::cerr << "  (" << hostname << "," << port << ")" << std::endl;
-      return -1;
-    }
-  
-    sockfd_own = socket(host_info_list->ai_family, 
-                        host_info_list->ai_socktype, 
-                        host_info_list->ai_protocol);
-    if (sockfd_own == -1) {
-      std::cerr << "Error: cannot create socket" << std::endl;
-      std::cerr << "  (" << hostname << "," << port << ")" << std::endl;
-      return -1;
-    }
-
-    int yes = 1;
-    status = setsockopt(sockfd_own, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
-    status = bind(sockfd_own, host_info_list->ai_addr, host_info_list->ai_addrlen);
-    if (status == -1) {
-      std::cerr << "Error: cannot bind socket" << std::endl;
-      std::cerr << "  (" << hostname << "," << port << ")" << std::endl;
-      return -1;
-    }
-
-    status = listen(sockfd_own, 100);
-    if (status == -1) {
-      std::cerr << "Error: cannot listen on socket" << std::endl; 
-      std::cerr << "  (" << hostname << "," << port << ")" << std::endl;
-      return -1;
-    }
-    freeaddrinfo(host_info_list);
-    return 0;
+  void closeSockfds(){
+    myKit.closeSockfds();
   }
-  /*
-  int selectPort(std::vector<int>& sockets, int ownNum, int playerNum){
-    int fdmax = *(std::max_element(sockets.begin(),sockets.end()));
-    fd_set sockfds;
-    FD_ZERO(&sockfds);
-    for(size_t i = 0; i < sockets.size(); i++){
-      FD_SET(sockets[i], &sockfds);
-    }
-    while(true){
-      fd_set sockfds_temp = sockfds; // copy it
-      if (select(fdmax+1, &sockfds_temp, NULL, NULL, NULL) == -1) {
-        std::perror("select");
-        return -1;
-      }
-      // run through the existing connections looking for data to read
-      for(int i = 0; i <= fdmax; i++) {
-        if (FD_ISSET(i, &sockfds_temp)) {
-          if (i == sockets[0]) { //ringmaster
-            struct potato p;
-            int gameStatus = getMsg(p,i);
-            if(gameStatus < 0){
-              std::cerr << "get potato from host error" << std::endl;
-              return -1;
-            }
-            else if(gameStatus > 0){
-              sendEnd(sockets,1);
-              sendEnd(sockets,2);
-              return 0;
-            }
-            int status = handlePotato(p,sockets,ownNum,playerNum);
-            if(status < 0){
-              std::cerr << "handle potato from host error" << std::endl;
-              return -1;
-            }
-          }
-          else{ //clients
-            struct potato p;
-            int gameStatusClient = getMsg(p,i);
-            if(gameStatusClient < 0){
-              std::cerr << "get potato from client error" << std::endl;
-              return -1;
-            }
-            else if(gameStatusClient > 0){
-              sendEnd(sockets,1);
-              sendEnd(sockets,2);
-              return 0;
-            }
-            int status = handlePotato(p,sockets,ownNum,playerNum);
-            if(status < 0){
-              std::cerr << "handle potato from client error" << std::endl;
-              return -1;
-            }
-          }
-        }
-      }
-    }
-    return 0;
-  }
-
-  */
-public:
-  ~Proxy(){}
 };
-/*
-  char myIP[16];
-  unsigned int myPort;
-  struct sockaddr_in my_addr;
-  bzero(&my_addr, sizeof(my_addr));
-  socklen_t len = sizeof(my_addr);
-  getsockname(socket_fd_own, (struct sockaddr *) &my_addr, &len);
-  inet_ntop(AF_INET, &my_addr.sin_addr, myIP, sizeof(myIP));
-  myPort = ntohs(my_addr.sin_port);
-
-  //std::cout << myIP << ":" << myPort << std::endl;
-  std::string portStr(std::to_string(myPort));
-  portStr += ';';
-  char const *pchar = portStr.c_str(); 
-  
-  if (send(sockfd, pchar, strlen(pchar), 0) == -1){ 
-  std::perror("send port number"); 
-  }
-  
-*/
