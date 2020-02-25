ab0a608d611d269bdfb3944199034567398b75f3
yz558 <yz558@duke.edu>
2020-02-25 17:53:31 -0500

    test

--- proxy.hpp
+++ proxy.hpp
@@ -1,60 +1,64 @@
 #include "Webtoolkit.hpp"
-#include "logger.hpp"
 #include "cache.hpp"
+#include "logger.hpp"
 
-class Proxy{
-private:
-  const char* hostname;
-  const char* port;
+class Proxy {
+ private:
+  const char * hostname;
+  const char * port;
   WebToolKit myKit;
   Timer myTimer;
   logger myLogger;
   Cache myCache;
   int ID;
-  
-public:
-  Proxy(const char* rhostname, const char* rport):hostname(rhostname), port(rport), myCache(20){}
 
-  int lisenClient(std::string& requestInfo){
-    if(myKit.listenBrowser(hostname, port)){
-      std::cerr <<"listen error!"<< std::endl;
+ public:
+  Proxy(const char * rhostname, const char * rport) :
+      hostname(rhostname),
+      port(rport),
+      myCache(20) {}
+
+  int lisenClient(std::string & requestInfo) {
+    if (myKit.listenBrowser(hostname, port)) {
+      std::cerr << "listen error!" << std::endl;
       return EXIT_FAILURE;
     }
 
-    while(requestInfo.empty()){
-      if(myKit.getRequest(requestInfo)){
+    while (requestInfo.empty()) {
+      if (myKit.getRequest(requestInfo)) {
         std::cerr << "get request error" << std::endl;
         return EXIT_FAILURE;
       }
     }
-    std::cout << "req: " <<requestInfo << std::endl;
+    std::cout << "req: " << requestInfo << std::endl;
     return EXIT_SUCCESS;
   }
-  
-  int handleGet(Request& reqObj){
+
+  int handleGet(Request & reqObj) {
     std::string cached = myCache.get(reqObj.getKey(), myTimer.getCurrentSec());
-    if(!cached.empty()){
+    if (!cached.empty()) {
       std::cout << "****************Cache: found*******************" << std::endl;
       //std::cout << cached << std::endl;
-      if(myKit.sendCacheBrowser(cached)){
-        std::cerr <<"get error!"<< std::endl;
+      if (myKit.sendCacheBrowser(cached)) {
+        std::cerr << "get error!" << std::endl;
         return EXIT_FAILURE;
       }
       myLogger.printCache("in cache", ID);
       /*in cache situations*/
       return EXIT_SUCCESS;
     }
-    else{
+    else {
       /*update cache*/
       std::string getInfo;
-      if(myKit.getServerSendBrowser(reqObj.getHostname(),reqObj.getRequestInfo(),getInfo)){
+      if (myKit.getServerSendBrowser(
+              reqObj.getHostname(), reqObj.getRequestInfo(), getInfo)) {
         return EXIT_FAILURE;
       }
       std::cout << getInfo << std::endl;
-      Response resObj(getInfo,reqObj.getType(),myTimer.getCurrentSec());
-      
-      if(resObj.canCache()){
-        myCache.put(reqObj.getKey(),resObj);
+      Response resObj(getInfo, reqObj.getType(), myTimer.getCurrentSec());
+
+      if (resObj.canCache()) {
+        myCache.put(reqObj.getKey(), resObj);
       }
       myLogger.printCache("not in cache", ID);
       myLogger.getrequest(ID, reqObj);
@@ -63,27 +67,28 @@ public:
     }
   }
 
-  int handlePost(Request& reqObj){
+  int handlePost(Request & reqObj) {
     std::string getInfo;
-    if(myKit.getServerSendBrowser(reqObj.getHostname(),reqObj.getRequestInfo(),getInfo)){
+    if (myKit.getServerSendBrowser(
+            reqObj.getHostname(), reqObj.getRequestInfo(), getInfo)) {
       return EXIT_FAILURE;
     }
     std::cout << getInfo << std::endl;
-    Response resObj(getInfo,reqObj.getType(),myTimer.getCurrentSec());
-    if(resObj.canCache()){
-      myCache.put(reqObj.getKey(),resObj);
+    Response resObj(getInfo, reqObj.getType(), myTimer.getCurrentSec());
+    if (resObj.canCache()) {
+      myCache.put(reqObj.getKey(), resObj);
     }
     return EXIT_SUCCESS;
   }
 
-  int handleConnect(Request& reqObj){
-    if(myKit.selectBrowserServer(reqObj.getHostname(),reqObj.getPort())){
+  int handleConnect(Request & reqObj) {
+    myLogger.getrequest(ID, reqObj);
+    if (myKit.selectBrowserServer(reqObj.getHostname(), reqObj.getPort())) {
       return EXIT_FAILURE;
     }
+    myLogger.printCache("Tunnel closed", ID);
     return EXIT_SUCCESS;
   }
 
-  void closeSockfds(){
-    myKit.closeSockfds();
-  }
+  void closeSockfds() { myKit.closeSockfds(); }
 };
