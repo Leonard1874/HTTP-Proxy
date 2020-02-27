a530698787c503b24856225d6a6c65995246b913
yz558 <yz558@duke.edu>
2020-02-26 23:09:14 -0500

    test

--- proxy.hpp
+++ proxy.hpp
@@ -3,15 +3,13 @@
 #include "logger.hpp"
 
 class Proxy {
-private:
+ private:
   const char * hostname;
   const char * port;
   WebToolKit myKit;
 
-public:
-  Proxy(const char * rhostname, const char * rport) : 
-    hostname(rhostname),
-    port(rport){}
+ public:
+  Proxy(const char * rhostname, const char * rport) : hostname(rhostname), port(rport) {}
 
   int lisenClient(std::string & requestInfo) {
     if (myKit.listenBrowser(hostname, port)) {
@@ -29,15 +27,19 @@ public:
     return EXIT_SUCCESS;
   }
 
-  int handleGet(Request & reqObj, Cache& myCache, Timer& myTimer, logger& myLogger, int& ID) {
-    
-    myLogger.getrequest_time(ID, reqObj); 
+  int handleGet(Request & reqObj,
+                Cache & myCache,
+                Timer & myTimer,
+                logger & myLogger,
+                int & ID) {
+    myLogger.getrequest_time(ID, reqObj);
     myLogger.print_recieve_requestline();
-    
+
     std::string cached = myCache.get(reqObj.getKey(), myTimer.getCurrentSec());
     std::cout << "****************Cached information*******************" << std::endl;
-    cout<<"******------********"<<cached<<endl;
-    if (cached != "notfound" && cached.find("revalidate:")==std::string::npos && cached[0] != 'I' && cached.find("expires:")==std::string::npos) {
+    cout << "******------********" << cached << endl;
+    if (cached != "notfound" && cached.find("revalidate:") == std::string::npos &&
+        cached[0] != 'I' && cached.find("expires:") == std::string::npos) {
       std::cout << "****************Cache: found*******************" << std::endl;
       //std::cout << cached << std::endl;
       if (myKit.sendCacheBrowser(cached)) {
@@ -45,14 +47,16 @@ public:
         return EXIT_FAILURE;
       }
       myLogger.printCache("in cache, valid", ID);
-      //respond
+      myLogger.getresponse_send(ID, cached);
+      myLogger.print_response_sendline();
     }
-    else if (cached == "notfound" || cached.find("expires:")!=std::string::npos) {
-      std::cout << "****************Cache: not found or expire*******************" << std::endl;
+    else if (cached == "notfound" || cached.find("expires:") != std::string::npos) {
+      std::cout << "****************Cache: not found or expire*******************"
+                << std::endl;
       /*update cache*/
       std::string getInfo;
       if (myKit.getServerSendBrowser(
-                                     reqObj.getHostname(), reqObj.getRequestInfo(), getInfo)) {
+              reqObj.getHostname(), reqObj.getRequestInfo(), getInfo)) {
         return EXIT_FAILURE;
       }
       std::cout << getInfo << std::endl;
@@ -61,29 +65,31 @@ public:
       if (resObj.canCache()) {
         myCache.put(reqObj.getKey(), resObj);
       }
-      if(cached == "notfound"){
+      if (cached == "notfound") {
         myLogger.printCache("not in cache", ID);
       }
-      else{
+      else {
         std::string expireString = "in cache, but expired at ";
-        cached = cached.substr(cached.find_first_of(':')+1);
+        cached = cached.substr(cached.find_first_of(':') + 1);
         long expireInt = std::stol(cached);
         expireString += myTimer.getlocalTimeStr(expireInt);
         myLogger.printCache(expireString, ID);
       }
       myLogger.getrequest_requesting(ID, reqObj);
       myLogger.print_send_requestline();
-      //recved
-      //responding
+      myLogger.getresponse_recieve(ID, resObj.getResponseInfo(), reqObj.getHostname());
+      myLogger.print_response_recieveline();
+      myLogger.getresponse_send(ID, resObj.getResponseInfo());
+      myLogger.print_response_sendline();
     }
     //Need revalidate
     else {
-      cached = cached.substr(cached.find_first_of(':')+1);
+      cached = cached.substr(cached.find_first_of(':') + 1);
       reqObj.setRequest(cached);
       std::string getInfo;
       cout << "*****************************Revalidate*****************\n";
       if (myKit.getServerSendBrowser(
-                                     reqObj.getHostname(), reqObj.getRequestInfo(), getInfo)) {
+              reqObj.getHostname(), reqObj.getRequestInfo(), getInfo)) {
         return EXIT_FAILURE;
       }
       std::cout << getInfo << std::endl;
@@ -96,13 +102,14 @@ public:
           return EXIT_FAILURE;
         }
         myLogger.printCache("NOTE revalidate success", ID);
-        //responding
+        myLogger.getresponse_send(ID, temp);
+        myLogger.print_response_sendline();
       }
-      else{
+      else {
         //Request a new response from the origin server
         std::string getInfo;
         if (myKit.getServerSendBrowser(
-                                       reqObj.getHostname(), reqObj.getRequestInfo(), getInfo)) {
+                reqObj.getHostname(), reqObj.getRequestInfo(), getInfo)) {
           return EXIT_FAILURE;
         }
         //std::cout << getInfo << std::endl;
@@ -113,20 +120,26 @@ public:
         myLogger.printCache("NOTE revalidate fail", ID);
         myLogger.getrequest_requesting(ID, reqObj);
         myLogger.print_send_requestline();
-        //recv
-        //respond
+        myLogger.getresponse_recieve(ID, resObj.getResponseInfo(), reqObj.getHostname());
+        myLogger.print_response_recieveline();
+        myLogger.getresponse_send(ID, resObj.getResponseInfo());
+        myLogger.print_response_sendline();
       }
     }
     return EXIT_SUCCESS;
   }
 
-  int handlePost(Request & reqObj, Cache& myCache, Timer& myTimer, logger& myLogger, int& ID) {
-    myLogger.getrequest_time(ID, reqObj); 
+  int handlePost(Request & reqObj,
+                 Cache & myCache,
+                 Timer & myTimer,
+                 logger & myLogger,
+                 int & ID) {
+    myLogger.getrequest_time(ID, reqObj);
     myLogger.print_recieve_requestline();
     std::string getInfo;
 
     if (myKit.getServerSendBrowser(
-                                   reqObj.getHostname(), reqObj.getRequestInfo(), getInfo)) {
+            reqObj.getHostname(), reqObj.getRequestInfo(), getInfo)) {
       return EXIT_FAILURE;
     }
     std::cout << getInfo << std::endl;
@@ -136,13 +149,15 @@ public:
     }
     myLogger.getrequest_requesting(ID, reqObj);
     myLogger.print_send_requestline();
-    //recv
-    //respond
+    myLogger.getresponse_recieve(ID, resObj.getResponseInfo(), reqObj.getHostname());
+    myLogger.print_response_recieveline();
+    myLogger.getresponse_send(ID, resObj.getResponseInfo());
+    myLogger.print_response_sendline();
     return EXIT_SUCCESS;
   }
 
-  int handleConnect(Request & reqObj, logger& myLogger, int& ID) {
-    myLogger.getrequest_time(ID, reqObj); 
+  int handleConnect(Request & reqObj, logger & myLogger, int & ID) {
+    myLogger.getrequest_time(ID, reqObj);
     myLogger.print_recieve_requestline();
     myLogger.getrequest_requesting(ID, reqObj);
     myLogger.print_send_requestline();
